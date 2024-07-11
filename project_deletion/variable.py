8e2e588809993149ea430a144fdc4b983b3be2cb
Yadav <abhishek.yadav01@sap.com>
2024-07-11 12:25:06 +0100

    added project deletion code

---
+++ project_deletion/variable.py
@@ -0,0 +1,10 @@
+import os
+from googleapiclient import discovery
+
+from oauth2client.client import GoogleCredentials
+
+projectid = os.getenv("Project_Id")
+
+credentials = GoogleCredentials.get_application_default()
+        
+service = discovery.build('compute', 'v1', credentials=credentials)
